bb24572603acc7fc7650302ce923b269eb1e0fc3
Dmitriy Galitsin <Simba16list@mail.ru>
2025-10-14 21:27:03 +0300

    Commit constructor

---
+++ constructor.php
@@ -0,0 +1,29 @@
+<?php
+
+class Cat
+{
+	private $name;
+	
+	public function __construct(string $name)
+	{
+		$this->name = $name;
+	}
+	
+	public function sayHello () : void
+	{
+		echo 'Привет! Меня зовут ' . $this->name . '.';
+	}
+	
+	public function setName (string $name) : void
+	{
+		$this->name = $name;
+	}
+	
+	public function getName () : string
+	{
+		return $this->name;
+	}
+}
+
+$cat1 = new Cat('Снежок');
+echo $cat1->getName();
